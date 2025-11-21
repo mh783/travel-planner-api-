5abd4a92ee39687febbe2cd1fdf290fbf3d387e7
mh783 <m.hernandezr2@uniandes.edu.co>
2025-11-21 10:27:02 -0500

    Logging middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,37 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  MiddlewareConsumer,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CountriesModule } from './countries/countries.module';
 import { TravelPlansModule } from './travel-plans/travel-plans.module';
+import { LoggingMiddleware } from './common/logging.middleware';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'travel.db',
-      autoLoadEntities: true, // carga todas las entidades *.entity.ts
+      autoLoadEntities: true,
       synchronize: true,
     }),
     CountriesModule,
     TravelPlansModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggingMiddleware)
+      .forRoutes(
+        // countries
+        { path: 'countries', method: RequestMethod.ALL },
+        { path: 'countries/:code', method: RequestMethod.ALL },
+        // travel plans (en tu app es 'plans')
+        { path: 'plans', method: RequestMethod.ALL },
+        { path: 'plans/:id', method: RequestMethod.ALL },
+      );
+  }
+}
